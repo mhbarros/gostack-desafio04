9dec59341654bddb1b739ab86b29c07bb1eac244
Marcelo Barros <mhbarros99@gmail.com>
2020-11-04 00:35:15 -0300

    Desafio 04 + cadastro de repositório

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 
 import {
   SafeAreaView,
@@ -10,46 +10,91 @@ import {
   TouchableOpacity,
 } from "react-native";
 
+import Api from './services/api';
+
 export default function App() {
-  async function handleLikeRepository(id) {
-    // Implement "Like Repository" functionality
+
+  const [repositories, setRepositories] = useState([]);
+
+  useEffect(() => {
+
+    Api.get('/repositories').then(response => {
+      setRepositories(response.data);
+    });
+
+  }, [])
+
+  const handleLikeRepository = async (id) => {
+    const response = await Api.post(`/repositories/${id}/like`);
+    let repList = repositories.map(rep => {
+      if(rep.id === id){
+        rep.likes = response.data.likes;
+      }
+      return rep;
+    });
+
+    setRepositories([...repList]);
+
+    console.log(repList);
+  }
+
+  const handleCreateRepository = async () => {
+    const projectNumber = Math.round(Math.random() * 1000);
+    const response = await Api.post('/repositories', {
+      title: `Projeto nº${projectNumber}`,
+      url: "https://github.com.br/mhbarros",
+      techs: ["Javascript", "React Native", "ReactJS"]
+    });
+
+    setRepositories([...repositories, response.data])
   }
 
   return (
     <>
       <StatusBar barStyle="light-content" backgroundColor="#7159c1" />
       <SafeAreaView style={styles.container}>
-        <View style={styles.repositoryContainer}>
-          <Text style={styles.repository}>Repository 1</Text>
+        <FlatList
+          data={repositories}
+          keyExtractor={repository => repository.id}
+          renderItem={({item: repository}) => (
 
-          <View style={styles.techsContainer}>
-            <Text style={styles.tech}>
-              ReactJS
-            </Text>
-            <Text style={styles.tech}>
-              Node.js
-            </Text>
-          </View>
+            <View style={styles.repositoryContainer}>
+              <Text style={styles.repository}>{repository.title}</Text>
 
-          <View style={styles.likesContainer}>
-            <Text
-              style={styles.likeText}
-              // Remember to replace "1" below with repository ID: {`repository-likes-${repository.id}`}
-              testID={`repository-likes-1`}
-            >
-              3 curtidas
-            </Text>
-          </View>
+              <View style={styles.techsContainer}>
+                {
+                  repository.techs.map(tech => (
+                    <Text key={tech} style={styles.tech}>
+                      {tech}
+                    </Text>
+                  ))
+                }
+              </View>
 
+              <View style={styles.likesContainer}>
+                <Text
+                  style={styles.likeText}
+                  testID={`repository-likes-${repository.id}`}
+                >
+                  {repository.likes} curtidas
+                </Text>
+              </View>
+
+              <TouchableOpacity
+                style={styles.button}
+                onPress={() => handleLikeRepository(repository.id)}
+                testID={`like-button-${repository.id}`}
+              >
+                <Text style={styles.buttonText}>Curtir</Text>
+              </TouchableOpacity>
+            </View>
+
+          )}  />
           <TouchableOpacity
-            style={styles.button}
-            onPress={() => handleLikeRepository(1)}
-            // Remember to replace "1" below with repository ID: {`like-button-${repository.id}`}
-            testID={`like-button-1`}
-          >
-            <Text style={styles.buttonText}>Curtir</Text>
+            style={styles.buttonSecondary}
+            onPress={() => handleCreateRepository()}>
+            <Text style={styles.buttonTextSecondary}>Cadastrar repositório</Text>
           </TouchableOpacity>
-        </View>
       </SafeAreaView>
     </>
   );
@@ -82,6 +127,7 @@ const styles = StyleSheet.create({
     paddingHorizontal: 10,
     paddingVertical: 5,
     color: "#fff",
+    borderRadius: 16
   },
   likesContainer: {
     marginTop: 15,
@@ -103,5 +149,20 @@ const styles = StyleSheet.create({
     color: "#fff",
     backgroundColor: "#7159c1",
     padding: 15,
+    borderRadius: 8
   },
+  buttonSecondary: {
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  buttonTextSecondary: {
+    fontSize: 14,
+    fontWeight: "bold",
+    marginRight: 10,
+    color: "#fff",
+    backgroundColor: "#04d361",
+    padding: 15,
+    marginBottom: 20,
+    borderRadius: 8
+  }
 });
